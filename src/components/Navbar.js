702076d8554858f97b7b03f758d917ec889884e8
Odair José Custodio Junior <odair.dev@gmail.com>
2022-04-05 18:58:48 -0300

    Reducer added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { SearchBar } from '../components/SearchBar'
+import { useTheme } from '../hooks/useTheme'
 import './Navbar.css'
 
-
 export function NavBar() {
+  const {color, changeColor} = useTheme()
+
   return (
-    <div className="navbar">
-      <nav>
+    <div className="navbar" style={{background: color}}>
+      <nav onClick={() => changeColor('pink')}>
         <Link to={'/'} className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,10 +1,27 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 
 export const ThemeContext = createContext()
 
+const ThemeReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return {...state, color: action.payload}
+    default:
+      return state
+  }
+}
+
 export const ThemeProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(ThemeReducer, {
+    color: 'blue'
+  })
+
+  const changeColor = (color) => {
+    dispatch({type: 'CHANGE_COLOR', payload: color})
+  }
+
   return (
-    <ThemeContext.Provider value={{color: 'blue'}}>
+    <ThemeContext.Provider value={{...state, changeColor}}>
       {children}
     </ThemeContext.Provider>
   )
