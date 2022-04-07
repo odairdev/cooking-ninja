2ba2b1fc49541a2615f13d5e733ce4cc55191b1b
Odair José Custodio Junior <odair.dev@gmail.com>
2022-04-07 14:55:39 -0300

    Minor changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,35 @@
 import { RecipeList } from "../../components/RecipeList"
-import { useFetch } from "../../hooks/useFetch"
+import { useState, useEffect } from 'react'
+import { projectFirestore } from "../../firebase/config"
 
 export function Home() {
-  const {data, pending, error} = useFetch('http://localhost:3000/recipes')
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').get().then(snapshot => {
+      if(snapshot.empty) {
+        setError('No recipes to load.')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({id: doc.id, ...doc.data()})
+        })
+        setData(results)
+        setIsPending(false)
+      }
+    }).catch(err => {
+      setError(err.message)
+    })
+  }, [])
 
   return (
     <div className="home">
-      {pending && <p className="loading">Loading...</p>}
+      {isPending && <p className="loading">Loading...</p>}
       {error && <p className="error">Could not fetch data.</p>}
       {data && <RecipeList recipes={data} />}
     </div>
--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -1,26 +1,44 @@
 import { useParams } from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch'
+import { useState, useEffect } from 'react'
 import { useTheme } from '../../hooks/useTheme'
 
 import './Recipe.css'
+import { projectFirestore } from '../../firebase/config'
 
 export function Recipe() {
   const { id } = useParams()
-  const {data: recipe, error, pending} = useFetch(`http://localhost:3000/recipes/${id}`)
   const { mode } = useTheme()
+
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').doc(id).get().then(doc => {
+      if(doc.exists) {
+        setIsPending(false)
+        setData(doc.data())
+      } else {
+        setIsPending(false)
+        setError('Could not find the recipe.')
+      }
+    })
+  }, [id])
 
   return (
     <div className={`recipe ${mode}`}>
-      {pending && <p className='loading'>Loading...</p>}
+      {isPending && <p className='loading'>Loading...</p>}
       {error && <p className='error'>Could not fetch data.</p>}
-      {recipe && (
+      {data && (
         <>
-          <h2 className={`page-title ${mode}`}>{recipe.title}</h2>
-          <p>Takes {recipe.cookingTime} to make.</p>
+          <h2 className={`page-title ${mode}`}>{data.title}</h2>
+          <p>Takes {data.cookingTime} to make.</p>
           <ul>
-            {recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+            {data.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
           </ul>
-          <p className="method">{recipe.method}</p>
+          <p className="method">{data.method}</p>
         </>
       )}
     </div>
